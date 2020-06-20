84845f7b88b13b6e53159d36fa84cd7282a934f6
3MenMovers-Owner <ninjas@3menmovers.com>
2020-06-20 07:33:15 -0500

    Updates

---
+++ precache-manifest.75480b0e8039d502fc8e3a069ee7b4c8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f7a1bff578e18a216d3a6ecff1b5fb",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "75543a58abdff99a65c8",
+    "url": "/react-pokedex/static/css/2.aebe04ed.chunk.css"
+  },
+  {
+    "revision": "026b4ba7674443922f1c",
+    "url": "/react-pokedex/static/css/main.22dfc985.chunk.css"
+  },
+  {
+    "revision": "75543a58abdff99a65c8",
+    "url": "/react-pokedex/static/js/2.10e2633c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pokedex/static/js/2.10e2633c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026b4ba7674443922f1c",
+    "url": "/react-pokedex/static/js/main.e8f8379e.chunk.js"
+  },
+  {
+    "revision": "8d0dc51f96d551a6d0f3",
+    "url": "/react-pokedex/static/js/runtime-main.f27a760f.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-pokedex/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-pokedex/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-pokedex/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-pokedex/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-pokedex/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
